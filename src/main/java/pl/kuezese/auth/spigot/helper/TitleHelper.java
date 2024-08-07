c1f87918c3704b05f98e4d3ddeda934a8056e4d6
cmclient <support@cmclient.pl>
2024-08-08 00:20:00 +0200

    replaced title api

--- src/main/java/pl/kuezese/auth/spigot/helper/TitleHelper.java
+++ src/main/java/pl/kuezese/auth/spigot/helper/TitleHelper.java
@@ -1,21 +1,11 @@
 package pl.kuezese.auth.spigot.helper;
 
-import net.minecraft.server.v1_8_R3.IChatBaseComponent;
-import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import com.connorlinfoot.titleapi.TitleAPI;
 import org.bukkit.entity.Player;
-import pl.kuezese.auth.shared.helper.ChatHelper;
 
 public class TitleHelper {
 
     public static void title(Player p, String up, String down, int i, int j, int k) {
-        IChatBaseComponent chatTitle = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + ChatHelper.color(up) + "\"}");
-        IChatBaseComponent chatSubTitle = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + ChatHelper.color(down) + "\"}");
-        PacketPlayOutTitle title = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, chatTitle);
-        PacketPlayOutTitle subtitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, chatSubTitle);
-        PacketPlayOutTitle length = new PacketPlayOutTitle(i, j, k);
-        ((CraftPlayer)p).getHandle().playerConnection.sendPacket(title);
-        ((CraftPlayer)p).getHandle().playerConnection.sendPacket(subtitle);
-        ((CraftPlayer)p).getHandle().playerConnection.sendPacket(length);
+        TitleAPI.sendTitle(p, i, j, k, up, down);
     }
 }
